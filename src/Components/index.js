0041956257f38dfff824db60ec31bd9a214fd4fa
Abrar Siddiqi <webdeveloping144@gmail.com>
2023-03-09 15:22:45 +0530

    first Commit Project

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,45 +1,67 @@
-import React, {  useState } from 'react'
+import React, {useState } from 'react'
 import './style.css'
 const Income = () => {
-     const [value,setValue] = useState('')
+  const [val,setVal]=useState('')
+  const [val1,setVal1]=useState('')
+  const [val2,setval2]=useState('')
+  const [val3,setval3]=useState('')
+  const [val4,setVal4]=useState('')
      const SubmitHandler = (e) =>{
-        e.preventDefault()
-        const income = (income) => {
-            let tax= 0;
-            if(income <= 300000){
-                tax = 0;
-            }else if(income > 300000 && income <= 600000){
-                tax = (income - 300000) *0.05;
-            }else if(income > 600000 && income <= 900000){
-                tax = (300000) * 0.05 + (income - 600000) * 0.10
-            }else if(income > 900000 && income <= 1200000){
-                tax = (300000) * 0.05 + (300000) * 0.10 + (income - 900000) * 0.15
-            }else if(income > 1200000 && income <= 1500000){
-                tax = (300000) * 0.05 + (300000) * 0.10 + ( 300000) * 0.15 + (income - 1200000) * 0.20
-            }else{
-                tax = (300000 * 0.05) + (300000 * 0.10) +  (300000 * 0.15) + (300000 * 0.20) + (income - 1500000) * 0.30
-            }
-            alert(`The income tax of an income of ${income} is: ${tax}`);
-        }
-        income(value)
+        e.preventDefault();
+        juneHadler('june');
+        SepHadler('sep');
+        DecHadler('dec');
+        MarchHadler('march')
      }
+     const juneHadler=(month)=>{
+      if(val>10000 && month==="june"){
+        let tax = (val*15/100);
+        setVal1(tax)
+      }
+    }
+    const SepHadler=(month)=>{
+      if(val>10000 && month==='sep'){
+        let tax = (val*45/100)-(val*15/100)
+        setval2(tax)
+      }
+    }
+    const DecHadler=(month)=>{
+      if(val>10000 && month==="dec"){
+        let tax = (val*75/100)-(val*45/100)
+        setval3(tax)
+      }
+    }
+    const MarchHadler=(month)=>{
+      if(val>10000 && month==="march"){
+        let tax = (val*100/100)-(val*75/100)
+        setVal4(tax)
+      }
+    }      
     return (
     <div>
          <div className="container">
-      <h2>Income Tax Calculator</h2>
-      <p>Revised Income tax slabs under new tax regime for FY 2023-24 (AY 2024-25)</p>
-      <p>
-        The income tax slabs under the new income tax regime will be as follows:
-      </p>
-      <table className="myWrapTable"><tbody><tr><td><strong>Income tax slabs under new tax regime</strong></td><td><strong>Income tax rates under new tax regime</strong></td></tr><tr><td>O to Rs 3 lakh</td><td>0</td></tr><tr><td>Rs 3 lakh to Rs 6 lakh</td><td>5%</td></tr><tr><td>Rs 6 lakh to Rs 9 lakh</td><td>10%</td></tr><tr><td>Rs 9 lakh to Rs 12 lakh</td><td>15%</td></tr><tr><td>Rs 12 lakh to Rs 15 lakh</td><td>20%</td></tr><tr><td>Income above Rs 15 lakh</td><td>30%</td></tr></tbody></table>
+      <h2>DUE Date Of Payment Of Advance Tax</h2>
+      <p>Revised Advance tax Slabs under new tax regime for FY 2023-24 </p>
+      <table className="myWrapTable"><tbody><tr><td><strong>Due date of payment of advance tax</strong></td><td><strong>For all assessee (except those assessee who has opted for Presumptive taxation scheme)</strong></td></tr>
+      <tr>
+        <td>Up to 15th June of financial year</td><td>15% of Tax Payable =: {`${val1}`}</td>
+        </tr>
+      <tr>
+        <td>Up to 15th September of financial year</td><td>45% of Tax Payable =: {`${val2}`}</td>
+      </tr>
+      <tr>
+        <td>Up to 15th December of financial year</td><td>75% of tax Payable =: {`${val3}`}</td>
+      </tr>
+      <tr><td>Up to 15th March of financial year</td><td>100% of tax Payable =: {`${val4}`}</td></tr>
+      </tbody></table>
 
-      <form id="tax-form" onSubmit={SubmitHandler}>
+      <form id="tax-form" onClick={SubmitHandler}>
         <div className="form-group">
           <label htmlFor="income">Annual Income: New Slab</label>
-            <input type="number" id="income" name="income" placeholder="Enter your annual income" required onChange={(e)=>setValue(e.target.value)} />
+            <input type="number" id="income" name="income" placeholder="Enter your annual income" required onChange={(e)=>setVal(e.target.value)} />
         </div>
-        <div className="form-group">
-          <button type="submit">Calculate Tax</button>
+        <div>
+           <button className='btn btn-primary text-white form-control' type='submit'>Submit</button>
         </div>
       </form>
       </div>
